828caa7cad10ae43d6ab12cf821efabb8c39f60f
John-Phealipto <johnadibe450@gmail.com>
2020-09-18 03:06:12 +0100

    Started the main page

--- site/js/script.js
+++ site/js/script.js
@@ -1,11 +1,11 @@
 // *** For hiding the menu on small screen when clicked on the body
-$(function () { // Same as document.addEventListener("DOMContentLoaded...")
+// $(function () { // Same as document.addEventListener("DOMContentLoaded...")
 
-	// Same as document.querySelector("#navbarToggle")
-	$("#navbarToggle").blur(function (event) {
-		var screenWidth = window.innerWidth;
-		if (screenWidth < 768) {
-			$("#mainNav").collapse('hide');
-		}
-	});
-});
+// 	// Same as document.querySelector("#navbarToggle")
+// 	$("#navbarToggle").blur(function (event) {
+// 		var screenWidth = window.innerWidth;
+// 		if (screenWidth < 768) {
+// 			$("#mainNav").collapse('hide');
+// 		}
+// 	});
+// });
